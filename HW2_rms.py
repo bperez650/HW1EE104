d2ce8cc21691600a14fa50890f8b0bc1fc4a9b50
bperez650 <39981852+bperez650@users.noreply.github.com>
2019-01-24 22:40:19 -0800

    HW 2 added comments and cleaned up

--- HW2_rms.py
+++ HW2_rms.py
@@ -1,15 +1,20 @@
+#########################################################
+#                                                       #
+# Will work for files with only numbers and dashes,     #
+# files with alpha chars will fail                      #
+#########################################################
+
 import numpy as np
 
 sum = 0 #variable for sum of squares
 index = 0   #index so we can take average
 
-with open("hw2_4.txt","r") as file:
+with open("hw2_3.txt","r") as file:
 
     for line in file:   #loop over all lines in file
         cast = float(line)  #parse out numerical characters and cast into floats
         sum += cast**2
         index  += 1
-
 
     sum /= index
     total = np.sqrt(sum)
