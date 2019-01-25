a745bc0eadd9c1fe3e3de353d9b4b3262e8f622f
bperez650 <39981852+bperez650@users.noreply.github.com>
2019-01-24 22:34:07 -0800

    hw 2

---
+++ HW2_rms.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+sum = 0 #variable for sum of squares
+index = 0   #index so we can take average
+
+with open("hw2_4.txt","r") as file:
+
+    for line in file:   #loop over all lines in file
+        cast = float(line)  #parse out numerical characters and cast into floats
+        sum += cast**2
+        index  += 1
+
+
+    sum /= index
+    total = np.sqrt(sum)
+    print(total)
+
+
+
+
